7739c374c8765fddf756215efba9ccddb2159159
alithegilfoyle <alimurtaza-assistant@outlook.com>
2022-07-14 09:44:39 +0500

    fix issues

--- contracts/test/MockV3Aggregator.sol
+++ contracts/test/MockV3Aggregator.sol
@@ -1,6 +1,5 @@
 // // SPDX-License-Identifier: MIT
 
-
 //we will define our own fake PriceFeed
 
 pragma solidity ^0.7.0;
@@ -10,6 +9,5 @@ import "@chainlink/contracts/src/v0.7/tests/MockV3Aggregator.sol";
 
 //nOte go to hardhat.config and add 0.7.0 version of solidity, so  we can compile different versions of solidity
 //module.exports ={
-   // compilers :[{version:"0.8.8"},{version:"0.7.0"}],
+// compilers :[{version:"0.8.8"},{version:"0.7.0"}],
 //}
-
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,32 +1,28 @@
 const networkConfig = {
-    //4= rinkeby chainId
-    4: {
-        name: "rinkeby",
-        ethUsdPriceFeed:"0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-        //with this we can keep track of different priceFeeds across different networks
-        
-    },
-137 : {
-
+  //4= rinkeby chainId
+  4: {
+    name: "rinkeby",
+    ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    //with this we can keep track of different priceFeeds across different networks
+  },
+  137: {
     //lets add PolyGon network
     //chainId of polygon =137
-      name: "polygon",
-      ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
-},
+    name: "polygon",
+    ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
+  },
+};
+// lets define our development chains
 
-}
- // lets define our development chains
-
- const  developmentChains = ["hardhat", "localhost"] //here we are using names not chainId
- //export developmentChain, add it module.export
-const DECIMALS = 8
-const INITIAL_ANSWER = 200000000000 //2000*10**8
+const developmentChains = ["hardhat", "localhost"]; //here we are using names not chainId
+//export developmentChain, add it module.export
+const DECIMALS = 8;
+const INITIAL_ANSWER = 200000000000; //2000*10**8
 //EXPORT BOTH DECIMALS AND INITIAL AN BELOW
 //we need to export the networkConfig to our script/deployfundme for it to run
 module.exports = {
-     networkConfig,
-     developmentChains,  //import ==> deploy-mocks with require
-     DECIMALS,
-     INITIAL_ANSWER,  // IMPORT==> deploy-mocks
-
-}
+  networkConfig,
+  developmentChains, //import ==> deploy-mocks with require
+  DECIMALS,
+  INITIAL_ANSWER, // IMPORT==> deploy-mocks
+};
